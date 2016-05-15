fe1ae7d5705616350db4154fd5b44abcf456eaf9
yingwei0831 <birneysky@163.com>
2016-05-15 12:19:42 +0800

    modify

---
+++ RP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RP
+//
+//  Created by birneysky on 16/5/14.
+//  Copyright © 2016年 birneysky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RP
+//
+//  Created by birneysky on 16/5/14.
+//  Copyright © 2016年 birneysky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
